8d3396bb0c4c5e6a331b00ba0d127a8d0e4b9a02
Mailan Franco <36058994+thesckurtt@users.noreply.github.com>
2025-05-24 20:49:21 -0300

    Create Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import bg_home from '../assets/video/bg_home.mp4'
+import logo_xxl from '../assets/img/logo-xxl.png'
+const Home = () => {
+  return (
+    <>
+      <video id="background-video" autoPlay loop muted playsInline>
+        <source src={bg_home} type="video/mp4" />
+        Seu navegador não suporta vídeos em HTML5.
+      </video>
+      <div className="overlay"></div>
+      <main className="main-site-home vh-100 vw-100 d-flex justify-content-center align-items-center flex-column">
+        <div className="logo-xxl mb-5">
+          <img src={logo_xxl} alt="Logo Talespawn" />
+        </div>
+        <div className="d-flex flex-column">
+          <a className="btn-rpg mb-3">Login</a>
+          <a className="btn-rpg">Register</a>
+        </div>
+      </main>
+    </>
+  )
+}
+
+export default Home
