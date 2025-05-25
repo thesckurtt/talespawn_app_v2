43a1a3add68742360f12afcedea14e080f468662
Mailan Franco <36058994+thesckurtt@users.noreply.github.com>
2025-05-25 14:44:54 -0300

    Update main.js

--- electron/main.js
+++ electron/main.js
@@ -1,19 +1,23 @@
-import {app, BrowserWindow, ipcMain} from 'electron'
+import { app, BrowserWindow, ipcMain } from 'electron'
 import path from 'path'
 
 let mainWindow = undefined
 
 const createMainWindow = () => {
   mainWindow = new BrowserWindow({
+    // fullscreen: true,
     width: 800,
-    height: 800,
+    height: 900,
+    simpleFullscreen: true,
     webPreferences: {
       nodeIntegration: false,
       contextIsolation: true,
-      // preload: path.resolve('preload.js')
+      preload: path.resolve('electron/preload.js')
     }
   })
+  mainWindow.webContents.openDevTools()
   mainWindow.setMenu(null)
+  // mainWindow.fullScreen = true
   mainWindow.loadURL('http://localhost:5173')
 }
 
@@ -24,6 +28,10 @@ app.whenReady().then(() => {
     if (BrowserWindow.getAllWindows().length === 0) createMainWindow();
   });
 });
+
+ipcMain.handle('actions-fullscreen', () => {
+  mainWindow.fullScreen = !mainWindow.fullScreen
+})
 
 // Sair do app quando todas as janelas forem fechadas (exceto no macOS)
 app.on('window-all-closed', function () {
