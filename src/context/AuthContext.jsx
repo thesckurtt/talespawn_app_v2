8b724ee2a556312729878485b713517d3dfee022
Mailan Franco <36058994+thesckurtt@users.noreply.github.com>
2025-05-25 19:41:38 -0300

    Update AuthContext.jsx

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -2,10 +2,26 @@ import { createContext, useContext, useState } from "react";
 
 const AuthContext = createContext()
 
-const AuthProvider = ({ children }) => {
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null)
+  const [token, setToken] = useState(null)
 
-
-  return <AuthContext.Provider>
+  function login({ email, password }) {
+    const data = {
+      email: email,
+      password: password
+    }
+    if (window.electronAuthAPI) {
+      const response = window.electronAuthAPI.login(data)
+      if (!response.error && response.token && response.user) {
+        setUser(response.user)
+        setToken(response.token)
+      }
+      return response
+    }
+    return { error: true }
+  }
+  return <AuthContext.Provider value={{ login }}>
     {children}
   </AuthContext.Provider>
 }
