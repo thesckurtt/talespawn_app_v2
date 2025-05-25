abae22406fd643e06994503f6228930ac889698e
Mailan Franco <36058994+thesckurtt@users.noreply.github.com>
2025-05-25 20:30:11 -0300

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,24 +2,30 @@ import React, { useState } from 'react'
 import logo_xxl from '../assets/img/logo-xxl.png'
 import InptGroup from '../components/InptGroup'
 import { useAuth } from '../context/AuthContext'
-
+import { useNavigate } from 'react-router-dom'
 const Login = () => {
   const [password, setPassword] = useState('')
   const [email, setEmail] = useState('')
-  const {login} = useAuth()
+  const [error, setError] = useState(null)
+  const navigate = useNavigate()
+  const { login } = useAuth()
   const addClassInptGroup = {
     container: "w-100",
     label: "w-100",
     input: "w-100",
   }
 
-  function handleSubmit(e) {
+  async function handleSubmit(e) {
     e.preventDefault()
     const data = {
       email: email,
       password: password
     }
-    login(data)
+    const response = await login(data)
+    console.log(response)
+    if (response.error) setError(true)
+    if (response.success) navigate('/game', {replace: true})
+
   }
 
   return (
@@ -29,6 +35,9 @@ const Login = () => {
       </div>
       <div className="d-flex flex-column justify-content-center align-items-center">
         <form onSubmit={(e) => handleSubmit(e)} className="d-flex flex-column justify-content-center align-items-center">
+          {error && <div class="alert alert-danger fw-medium" role="alert">
+            Credênciais inválidas! Tente novamente.
+          </div>}
           <InptGroup addClass={addClassInptGroup} name={'email'} type={'text'} label={'Email'} value={email} handleChange={setEmail} />
           <InptGroup addClass={addClassInptGroup} name={'password'} type={'password'} label={'Password'} value={password} handleChange={setPassword} />
           <button className="btn-rpg mt-4 mb-3" type="submit">Login</button>
