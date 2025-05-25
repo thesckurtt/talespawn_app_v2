b055fac4261d8ba5970cd6bdd92446d3aa3f7b5c
Mailan Franco <36058994+thesckurtt@users.noreply.github.com>
2025-05-25 20:30:13 -0300

    Update AuthContext.jsx

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -5,23 +5,40 @@ const AuthContext = createContext()
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null)
   const [token, setToken] = useState(null)
+  const [isLoggedIn, setIsLoggedIn] = useState(() => {
+    return localStorage.getItem('isLoggedIn') === 'true';
+  })
 
-  function login({ email, password }) {
+  async function login({ email, password }) {
     const data = {
       email: email,
       password: password
     }
     if (window.electronAuthAPI) {
-      const response = window.electronAuthAPI.login(data)
+      const response = await window.electronAuthAPI.login(data)
       if (!response.error && response.token && response.user) {
+        // console.log(response.token)
         setUser(response.user)
         setToken(response.token)
+        setIsLoggedIn(true);
+        localStorage.setItem('user', response.user);
+        localStorage.setItem('authToken', response.token)
+        localStorage.setItem('isLoggedIn', 'true');
       }
       return response
     }
     return { error: true }
   }
-  return <AuthContext.Provider value={{ login }}>
+
+  function logout() {
+    setUser(null);
+    setToken(null);
+    setIsLoggedIn(false);
+    localStorage.removeItem('authToken');
+    localStorage.removeItem('isLoggedIn');
+  }
+
+  return <AuthContext.Provider value={{ login, logout }}>
     {children}
   </AuthContext.Provider>
 }
