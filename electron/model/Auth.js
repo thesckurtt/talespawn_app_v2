75498c4b6260461594a9a27b0fe35da479668a59
Mailan Franco <36058994+thesckurtt@users.noreply.github.com>
2025-05-25 19:01:35 -0300

    Update Auth.js

--- electron/model/Auth.js
+++ electron/model/Auth.js
@@ -1,41 +1,66 @@
 // import db from "../database/db";
 import bcrypt from "bcryptjs";
-import { User } from "./User";
+import { User } from "./User.js";
 import jwt from 'jsonwebtoken'
 import dotenv from 'dotenv';
 import Joi from "joi";
+import { Validate } from "./Validate.js";
 dotenv.config()
 
+
+
 export class Auth {
-  static login({ email, password }) {
+  /**
+   * Realiza login de usuário verificando email e senha.
+   * Retorna token JWT se sucesso.
+   * @param {string} email
+   * @param {string} password
+   * @returns {Object} resultado do login
+   */
+  static async login({ email, password }) {
     try {
-      const user = User.getUserByEmail(email)
-      const isValidHash = bcrypt.compare(password, user.password)
+      const user = await User.getUserByEmail(email)
+
+      const isValidHash = user ? await bcrypt.compare(password, user.password) : false
+
       if (isValidHash) {
         const token = jwt.sign({ user_id: user.id }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRES_IN })
         return { success: true, token: token }
       }
+
       return { error: true }
+
     } catch (error) {
-      throw new Error("Erro: ", error)
+      return { error: true, message: error }
     }
   }
-  static register({ name, email, nickname, password }) {
-    // const schema = Joi.object({
-    //   name: Joi.string().min(3).required(),
-    //   email: Joi.string().email().required(),
-    //   nickname: Joi.string().min(3).required(),
-    //   password: Joi.string().min(3).required()
-    // })
 
-    // const { error } = schema.validate({ name, email, nickname, password })
+  /**
+   * Registra um novo usuário.
+   * @param {string} name
+   * @param {string} email
+   * @param {string} nickname
+   * @param {string} password
+   * @returns {Object} resultado do cadastro
+   */
+  static async register({ name, email, nickname, password }) {
+    try {
+      const user = await User.addUser({ name, email, nickname, password })
+      console.log(user)
+      return { error: false, user_id: user.user_id }
+    } catch (error) {
+      return { error: true, message: error }
+    }
 
-    // if (error) return { error: true }
-
-    // try {
-      
-    // } catch (error) {
-    //   throw new Error("Error: ", error)
-    // }
   }
 }
+
+// const user = {
+//   name: "dasdas",
+//   email: "testetet@gail.com",
+//   nickname: "tetetet",
+//   password: "teste2"
+// }
+
+// Auth.register(user).then(res => console.log(res))
+// Auth.login(user).then(res => console.log(res))
