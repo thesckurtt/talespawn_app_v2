884e195ee0fb05896ce413705ba1379d11b19639
Mailan Franco <36058994+thesckurtt@users.noreply.github.com>
2025-05-25 16:00:04 -0300

    Create db.js

---
+++ electron/database/db.js
@@ -0,0 +1,24 @@
+import knex from "knex";
+import path from "path"
+
+const db = knex({
+  client: 'sqlite3',
+  connection: {
+    filename: path.resolve('electron/database/data.db')
+  },
+  useNullAsDefault: true
+})
+
+db.schema.hasTable('users').then(exists => {
+  if (!exists) {
+    return db.schema.createTable('users', table => {
+      table.increments('id').primary()
+      table.string('name')
+      table.string('email').unique()
+      table.string('nickname').unique()
+      table.string('password')
+    })
+  }
+})
+
+export default db
