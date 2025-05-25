e4ab6762b7d009e6ac875627f642b25bf54b399d
Mailan Franco <36058994+thesckurtt@users.noreply.github.com>
2025-05-25 14:45:07 -0300

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -6,17 +6,22 @@ const Login = () => {
   const [password, setPassword] = useState('')
   const [email, setEmail] = useState('')
 
+  const addClassInptGroup = {
+    container: "w-100",
+    label: "w-100",
+    input: "w-100",
+  }
 
   return (
-    <main class="main-site-home vh-100 vw-100 d-flex justify-content-center align-items-center flex-column">
-      <div class="logo-xxl mb-5">
+    <main className="main-site-login vh-100 vw-100 d-flex justify-content-center align-items-center flex-column">
+      <div className="logo-xxl mb-5">
         <img src={logo_xxl} alt="Logo Talespawn" />
       </div>
-      <div class="d-flex flex-column justify-content-center align-items-center">
-        <form action="" class="d-flex flex-column justify-content-center align-items-center">
-          <InptGroup name={'email'} type={'text'} label={'Email'} value={email} handleChange={setEmail} />
-          <InptGroup name={'password'} type={'password'} label={'Password'} value={password} handleChange={setPassword} />
-          <button class="btn-rpg mb-3" type="submit">Login</button>
+      <div className="d-flex flex-column justify-content-center align-items-center">
+        <form action="" className="d-flex flex-column justify-content-center align-items-center">
+          <InptGroup addClass={addClassInptGroup} name={'email'} type={'text'} label={'Email'} value={email} handleChange={setEmail} />
+          <InptGroup addClass={addClassInptGroup} name={'password'} type={'password'} label={'Password'} value={password} handleChange={setPassword} />
+          <button className="btn-rpg mt-4 mb-3" type="submit">Login</button>
         </form>
       </div>
     </main>
