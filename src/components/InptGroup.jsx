6a7493c92d9ea16685c99d98fe3b1688d415b219
Mailan Franco <36058994+thesckurtt@users.noreply.github.com>
2025-05-25 14:45:02 -0300

    Update InptGroup.jsx

--- src/components/InptGroup.jsx
+++ src/components/InptGroup.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const InptGroup = ({ name, type, label, handleChange, value }) => {
+const InptGroup = ({ addClass, name, type, label, handleChange, value }) => {
   return (
-    <div className="d-flex flex-column justify-content-start align-items-start mb-3">
-      <label htmlFor={name} className="label-rpg fs-3">{label}</label>
-      <input type={type} id={name} name={name} className="inpt-rpg" value={value} onChange={(e) => handleChange(e.target.value)} />
+    <div className={`d-flex flex-column justify-content-start align-items-start mb-3 ${addClass.container}`}>
+      <label htmlFor={name} className={`label-rpg fs-3  ${addClass.label}`}>{label}</label>
+      <input type={type} id={name} name={name} className={`inpt-rpg ${addClass.input}`} value={value} onChange={(e) => handleChange(e.target.value)} />
     </div>
   )
 }
