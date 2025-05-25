81f14cf8a26422740a34429994f0a610bcecb53f
Mailan Franco <36058994+thesckurtt@users.noreply.github.com>
2025-05-25 14:44:57 -0300

    Update preload.js

--- electron/preload.js
+++ electron/preload.js
@@ -1,6 +1,10 @@
-import { contextBridge, ipcRenderer } from 'electron'
+const { contextBridge, ipcRenderer } = require('electron')
 
 contextBridge.exposeInMainWorld('electronAuthAPI', {
   login: (credentials) => ipcRenderer.invoke('login', credentials),
   register: (credentials) => ipcRenderer.invoke('register', credentials),
+})
+
+contextBridge.exposeInMainWorld('electronActionsAPI', {
+  fullscreen: () => ipcRenderer.invoke('actions-fullscreen')
 })
