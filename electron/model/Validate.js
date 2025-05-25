003c792ca27fff78f16097ad79e969047cb6b7bf
Mailan Franco <36058994+thesckurtt@users.noreply.github.com>
2025-05-25 18:40:32 -0300

    Update Validate.js

--- electron/model/Validate.js
+++ electron/model/Validate.js
@@ -1,6 +1,20 @@
 import Joi from "joi";
 
+/**
+ * Classe Validate responsável por validar dados 
+ * usando regras definidas com Joi.
+ */
 export class Validate {
+  /**
+   * Valida os dados do usuário para cadastro.
+   * @param {string} name Nome do usuário.
+   * @param {string} email Email do usuário.
+   * @param {string} nickname Apelido do usuário.
+   * @param {string} password Senha do usuário.
+   * @returns {Object} Objeto contendo status da validação.
+   * @returns {boolean} retorno.valid Indica se os dados são válidos.
+   * @returns {string} [retorno.message] Mensagem de erro caso inválido.
+   */
   static validateUser({ name, email, nickname, password }) {
     const schema = Joi.object({
       name: Joi.string().min(3).required(),
@@ -9,8 +23,25 @@ export class Validate {
       password: Joi.string().min(3).required()
     })
     const { error } = schema.validate({ name, email, nickname, password })
-    // console.log(error)
-    if (error) return { valid: false, message: error.details[0].message}
+    if (error) return { valid: false, message: error.details[0].message }
+    return { valid: true }
+  }
+
+  /**
+    * Valida os dados de login.
+    * @param {string} email Email do usuário.
+    * @param {string} password Senha do usuário.
+    * @returns {Object} Objeto contendo status da validação.
+    * @returns {boolean} retorno.valid Indica se os dados são válidos.
+    * @returns {string} [retorno.message] Mensagem de erro caso inválido.
+    */
+  static validateLogin({ email, password }) {
+    const schema = Joi.object({
+      email: Joi.string().email().required(),
+      password: Joi.string().min(3).required()
+    })
+    const { error } = schema.validate({ email, password })
+    if (error) return { valid: false, message: error.details[0].message }
     return { valid: true }
   }
 }
