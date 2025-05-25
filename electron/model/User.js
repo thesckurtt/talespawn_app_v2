1257d8c5b90052fe980b8db5ae5b6fde9c7c57f8
Mailan Franco <36058994+thesckurtt@users.noreply.github.com>
2025-05-25 18:10:10 -0300

    Update User.js

--- electron/model/User.js
+++ electron/model/User.js
@@ -1,8 +1,37 @@
 import db from "../database/db.js";
+import Joi from "joi";
+
 export class User {
   static async getAllUsers() {
     try {
       return await db('users').select('*')
+    } catch (error) {
+      throw new Error("Error: ", error)
+    }
+  }
+  static addUser({ name, email, nickname, password }) {
+    const schema = Joi.object({
+      name: Joi.string().min(3).required(),
+      email: Joi.string().email().required(),
+      nickname: Joi.string().min(3).required(),
+      password: Joi.string().min(3).required()
+    })
+
+    const { error } = schema.validate({ name, email, nickname, password })
+
+    if (error) return { error: true }
+    
+    try {
+      db('users').insert()
+    } catch (error) {
+      throw new Error("Error: ", error)
+    }
+  }
+
+
+  static async getUserByEmail(email) {
+    try {
+      return await db('users').where('email', email).first()
     } catch (error) {
       throw new Error("Error: ", error)
     }
