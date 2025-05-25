b6b1ea2534a6399403e089fac1bd64bcc1915ed4
Mailan Franco <36058994+thesckurtt@users.noreply.github.com>
2025-05-25 14:45:01 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Register from './pages/Register'
+import Login from './pages/Login'
+import Game from './pages/Game'
 
 const App = () => {
+  useEffect(()=>{
+    document.addEventListener('keydown', (e) => {
+      if(e.key === "F11"){
+        window.electronActionsAPI.fullscreen()
+      }
+    })
+  })
   return (
     <Router basename='/'>
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/login' element={<h1>login /</h1>} />
+        <Route path='/login' element={<Login />} />
         <Route path='/register' element={<Register />} />
-        <Route path='/game' element={<h1>game /</h1>} />
+        <Route path='/game' element={<Game />} />
       </Routes>
     </Router>
   )
