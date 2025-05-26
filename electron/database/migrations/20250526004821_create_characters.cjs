5bbd657ce62908838597e1e52153c567445f9829
Mailan Franco <36058994+thesckurtt@users.noreply.github.com>
2025-05-26 10:48:39 -0300

    Update 20250526004821_create_characters.cjs

--- electron/database/migrations/20250526004821_create_characters.cjs
+++ electron/database/migrations/20250526004821_create_characters.cjs
@@ -1,17 +1,25 @@
 
 exports.up = function (knex) {
-  knex.schema.hasTable('characters').then((exists) => {
+  return knex.schema.hasTable('characters').then((exists) => {
     if (!exists) {
       return knex.schema.createTable('characters', (table) => {
-        table.increments('id').primary();
-        table.string('name').notNullable();
-        table.string('image').notNullable();
-        table.string('description').notNullable();
+        table.increments('id').primary()
+        table.string('name').notNullable()
+        table.string('image').notNullable()
+        table.string('bg_video').notNullable()
+        table.string('main_audio').notNullable()
+        table.string('description').notNullable()
+
+        // Attributes
+        table.integer('magic').notNullable().defaultTo(0)
+        table.integer('attack').notNullable().defaultTo(0)
+        table.integer('healing').notNullable().defaultTo(0)
+        table.integer('perception').notNullable().defaultTo(0)
       })
     }
   })
 };
 
 exports.down = function (knex) {
-return knex.schema.dropTableIfExists('characters');
+  return knex.schema.dropTableIfExists('characters');
 };
