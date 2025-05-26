f040632b5dc31a5f33cab544610f5e4caaed8b63
Mailan Franco <36058994+thesckurtt@users.noreply.github.com>
2025-05-26 10:48:38 -0300

    Update 20250526003815_create_users_table.cjs

--- electron/database/migrations/20250526003815_create_users_table.cjs
+++ electron/database/migrations/20250526003815_create_users_table.cjs
@@ -3,11 +3,11 @@ exports.up = function (knex) {
   return knex.schema.hasTable('users').then(function (exists) {
     if (!exists) {
       return knex.schema.createTable('users', function (table) {
-        table.increments('id').primary();
-        table.string('name').notNullable();
-        table.string('email').unique().notNullable();
-        table.string('nickname').unique().notNullable();
-        table.string('password').notNullable();
+        table.increments('id').primary()
+        table.string('name').notNullable()
+        table.string('email').unique().notNullable()
+        table.string('nickname').unique().notNullable()
+        table.string('password').notNullable()
 
         // Como enum não é suportado nativamente no SQLite, você pode usar string ou integer:
         // table.enum('character_id', ['1', '2', '3']).notNullable().defaultTo('1');
