2477f786c6aa471756708ec9202d7574fdb60a9b
Mailan Franco <36058994+thesckurtt@users.noreply.github.com>
2025-05-28 10:03:27 -0300

    fix bug

--- electron/database/knexfile.cjs
+++ electron/database/knexfile.cjs
@@ -5,13 +5,13 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: path.resolve('./data.db')
+      filename: path.resolve(__dirname, './data.db')
     },
     migrations: {
-      directory: path.resolve('./migrations')
+      directory: path.resolve(__dirname, './migrations')
     },
     seeds: {
-      directory: path.resolve('./seeds')
+      directory: path.resolve(__dirname, './seeds')
     },
     useNullAsDefault: true,
     pool: {
