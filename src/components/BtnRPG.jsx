380387856854f11f67c828a261970e319be2ea2b
Mailan Franco <36058994+thesckurtt@users.noreply.github.com>
2025-06-25 23:04:45 -0300

    Update BtnRPG.jsx

--- src/components/BtnRPG.jsx
+++ src/components/BtnRPG.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
-export const BtnRPG = ({ label, handleClick }) => {
-  return <button onClick={handleClick} className="btn-rpg">{label}</button>;
+export const BtnRPG = ({ label, handleClick, option }) => {
+  return <button onClick={() => handleClick(option)} className="btn-rpg">{label}</button>;
 };
